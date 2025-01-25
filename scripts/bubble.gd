2d0f7148a164ae8d21a008160973a1000b1abef5
Driannn <drianbetancourt@gmail.com>
2025-01-25 14:03:00 -0300

    gameplay

--- scripts/bubble.gd
+++ scripts/bubble.gd
@@ -1,9 +1,11 @@
 extends RigidBody2D
 
+@export var min_speed: float
+@export var max_speed: float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	gravity_scale = randf_range(-0.3, -1)
+	gravity_scale = randf_range(-min_speed, -max_speed)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
