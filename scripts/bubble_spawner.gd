63e320ab7fd140a72dcd880f43f144ca8b603aec
Driannn <drianbetancourt@gmail.com>
2025-01-26 08:39:18 -0300

    Random colors on bubbles

--- scripts/bubble_spawner.gd
+++ scripts/bubble_spawner.gd
@@ -2,7 +2,9 @@ extends Node2D
 
 signal bubble_spawned(bubble_instance)
 
-var bubble_scene = preload("res://scenes/bubble.tscn")
+@onready var bubble_scene = preload("res://scenes/bubble.tscn")
+
+@export var colors: Array[Color]
 
 @export var spawn_time : float = 2.5
 
@@ -16,12 +18,17 @@ func _ready():
 func spawn_bubble():
 	var bubble_instance = bubble_scene.instantiate()
 	bubble_instance.global_position.x = randf_range(pos_range[0].global_position.x,pos_range[1].global_position.x)  
+	set_bubble_color(bubble_instance)
 	emit_signal("bubble_spawned", bubble_instance)
 	add_child(bubble_instance)
 
 func _on_timer_timeout() -> void:
 	spawn_bubble()
-#
+
+func set_bubble_color(instance: RigidBody2D):
+	instance.modulate = colors.pick_random()
+
+
 #func _on_path_bubble_spawn_timeout():
 	#spawn_path_bubble()
 #
